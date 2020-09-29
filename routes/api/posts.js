486ae619a66aff71eac9f20cd763df4d18cf2f76
Milan Milenkovic <milenkovicmilan10@gmail.com>
2020-09-29 19:19:24 +0200

    :fire:

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -35,4 +35,90 @@ router.post(
   }
 );
 
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+});
+
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.json(post);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server error");
+  }
+});
+
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+    await post.remove();
+
+    res.json({ msg: "Post has been removed" });
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server error");
+  }
+});
+
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+        const post = await Post.findById(req.params.id)
+     if(post.likes.filter(like=>like.user.toString() === req.user.id).length > 0)   {
+             return res.status(400).json({msg:"Post already liked"})
+     }
+
+     post.likes.unshift({user:req.user.id})
+     await post.save()
+     res.json(post.likes)
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+});
+
+
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+        const post = await Post.findById(req.params.id)
+     if(post.likes.filter(like=>like.user.toString() === req.user.id).length === 0)   {
+             return res.status(400).json({msg:"Post has not yet been liked"})
+     }
+
+     const removeIndex = post.likes.map(like=>like.user.toString()).indexOf(req.user.id)
+
+     post.likes.splice(removeIndex, 1)
+
+     await post.save()
+     res.json(post.likes)
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
